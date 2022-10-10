d95392a9ff180266a58005127fea2865ca4a55b0
Pranto-Sen <83857088+Pranto-Sen@users.noreply.github.com>
2022-10-11 00:02:57 +0600

    Create LabTask2.php

---
+++ Lab_Task 02/LabTask2.php
@@ -0,0 +1,160 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
+
+<?php
+
+$nameErr = $emailErr = $degreeErr = $genderErr = "";
+$name = $email = $gender = "";
+$successmsg = "";
+
+$errCount = 0;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+    $errCount = $errCount + 1;	
+  } else {
+    $name = test_input($_POST["name"]);
+    $wcount = str_word_count($name);
+    if ($wcount <2 ) {
+   
+    	$nameErr = "Minimum 2 words required";
+    	$errCount = $errCount + 1;	
+    }
+
+   
+    if (!preg_match("/^[a-zA-Z]/", $name)) {
+    	$nameErr = "Name must start with a letter!";
+    	$name ="";
+    	$errCount = $errCount + 1;	
+    }
+
+    if (!preg_match("/^[a-zA-Z_\-. ]*$/",$name)) {
+      $nameErr = "Only letters, period and white space allowed";
+      $name="";
+      $errCount = $errCount + 1;	
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+    $errCount = $errCount + 1;	
+  } else {
+    $email = test_input($_POST["email"]);
+   
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+      $email="";
+      $errCount = $errCount + 1;	
+    }
+  }
+
+  
+
+
+  if (empty($_POST['bgrp'])) {
+  	$bgrpErr = "Must select a blood group";
+  	$errCount = $errCount + 1;	
+  } else {
+  	$bgrp = test_input($_POST["bgrp"]);
+  }
+
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+    $errCount = $errCount + 1;	
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+
+  if ($errCount < 1) {
+	$successmsg = "Successfully submitted the form without any error.";
+}
+
+
+if ($successmsg) {
+	echo $successmsg;
+} else {
+	echo '<span class="error">One or more error occurred!</span>';
+}
+
+}
+
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error"> <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error"> <?php echo $emailErr;?></span>
+  <br><br>
+
+
+	Date Of Birth: <br>
+	dd <input type="text" size="5" name="dobdd" > mm <input type="text" size="5" name="dobmm"> 
+	yy <input type="text" name="dobyy" size="5" >
+  
+  <br><br>
+
+
+  Gender:
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+  <span class="error"> <?php echo $genderErr;?></span>
+  <br><br>
+
+  Degree:
+	<input type="checkbox" id="ssc" name="ssc" value="SSC">
+  <label for="ssc"> SSC</label>
+  <input type="checkbox" id="hsc" name="hsc" value="HSC">
+  <label for="hsc"> HSC</label>
+  <input type="checkbox" id="bsc" name="bsc" value="BSc">
+  <label for="bssc"> BSc</label>
+  <input type="checkbox" id="vehicle3" name="msc" value="MSc">
+  <label for="msc"> MSc</label>
+  <span class="error"><?php echo $degreeErr;?></span>
+  <br><br>
+
+
+  <label for="cars">Blood Group:</label>
+  <select name="bgrp" id="bgrp">
+  	<option value="" selected> </option>
+    <option value="O+">O+</option>
+    <option value="A+">A+</option>
+    <option value="B+">B+</option>
+  </select>
+  <span class="error"></span>
+  <br><br>
+
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<br>";
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $bgrp;
+?>
+
+</body>
+</html>
